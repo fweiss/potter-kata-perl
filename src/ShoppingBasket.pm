e93fdd86451532d170b5419a3363b1210854931b
Fr2d9y2 <frank_weiss@gap.com>
2015-06-04 15:23:42 -0700

    Add single book test.

--- src/ShoppingBasket.pm
+++ src/ShoppingBasket.pm
@@ -2,13 +2,20 @@ package ShoppingBasket;
 
 sub new() {
     my $class = shift;
-    my $self = {};
+    my $self = {
+        items => []
+    };
     bless $self, $class;
     return $self;
 }
 
 sub getTotal() {
-    return 0;
+    return 8.00 * ($#{self->{items}} + 1);
+}
+
+sub addBook() {
+    my $title = shift;
+    push @{self->{items}}, $title;
 }
 
 1;
--- src/potter-test.pl
+++ src/potter-test.pl
@@ -4,6 +4,8 @@ use ShoppingBasket;
 
 $basket = ShoppingBasket->new();
 
-is($basket->getTotal(), 0, "empty basket");
+is($basket->getTotal(), 0.00, "empty basket");
+$basket->addBook("first");
+is($basket->getTotal(), 8.00, "one book");
 
 done_testing();
